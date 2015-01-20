d7de3b2715eba5572f2742cda31c48d322f7d30f
joskel <j.kelleners@maasbracht.org>
2015-01-20 21:31:00 +0000

    filling in models

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,17 @@ class Appointment < ActiveRecord::Base
   belongs_to :schedule
   belongs_to :user  
   validates_presence_of :date, :start_time, :stop_time
+  
+ 
+  
+    def in_schedule?
+      if schedule.weekday == date.cwday 
+      if ((start_time > schedule.start_time_1) && (stop_time < schedule.stop_time_1)) || ((start_time > schedule.start_time_2) && (stop_time < schedule.stop_time_2))
+        
+        return true
+      end 
+      end
+      false 
+    end  
+  
 end
